31d72598827c6bf2fdafc7ab92be903d956e2c44
Sahilll <150071026+sahilsharda@users.noreply.github.com>
2026-03-31 22:54:18 +0530

    Change name to Sahil Mankhi

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
     <div className="hero-section">
       <div className="hero-content">
         <h3 className="hero-greeting">Hi, my name is</h3>
-        <h1 className="hero-name">Jane Doe.</h1>
+        <h1 className="hero-name">Sahil Mankhi.</h1>
         <h1 className="hero-subtitle">I build things for the web.</h1>
         <p className="hero-desc">
           I'm a software engineer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I'm focused on building accessible, human-centered products.
